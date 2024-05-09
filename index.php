9afa3bdf0d45c4284185dfcc496510267976ee1a
Arlan Tri Handika <handikaarlan@gmail.com>
2024-05-09 13:17:57 +0700

    minor update

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ function simpanData($todos)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-do App</title>
+    <title>Simple To-do App</title>
     <style>
         body {
             font-family: Arial, sans-serif;
